d11cde1d4b36f487884783f6857926d059ab1d88
yuan-0816 <wj582693@gmail.com>
2025-12-03 17:37:25 +0800

    目錄結構更新 and 自動下載最新brdc

---
+++ src/hackrf/fake_gps.py
@@ -0,0 +1,13 @@
+
+
+class FakeGPS:
+    def __init__(self):
+
+        pass
+
+
+
+
+
+if __name__ == "__main__":
+    pass
--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from tools.fetch_brdc import fetch_latest_ephemeris
 
+
+path = fetch_latest_ephemeris()
 
 
 def main():
